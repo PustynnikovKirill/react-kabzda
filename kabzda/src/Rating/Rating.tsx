1a641182341078cd9a5694b04ee0858741f05e8e
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-05-25 10:02:45 +0300

    'star'

--- kabzda/src/Rating/Rating.tsx
+++ kabzda/src/Rating/Rating.tsx
@@ -1,27 +1,31 @@
 import React, {useState} from 'react';
 
-export const UnControledRating =()=>{
-    let [value,setValue] = useState(0)
+export const UnControledRating = () => {
+    let [value, setValue] = useState(0)
     return (
         <div>
-                <Star selected = {value > 0}/><button onClick={()=>{setValue(1)}}>1</button>
-                <Star selected = {value > 1}/><button onClick={()=>{setValue(2)}}>2</button>
-                <Star selected = {value > 2}/><button onClick={()=>{setValue(3)}}>3</button>
-                <Star selected = {value > 3}/><button onClick={()=>{setValue(4)}}>4</button>
-                <Star selected = {value > 4}/><button onClick={()=>{setValue(5)}}>5</button>
+            <Star value={1} selected={value > 0} setValue={setValue}/>
+            <Star value={2} selected={value > 1} setValue={setValue}/>
+            <Star value={3} selected={value > 2} setValue={setValue}/>
+            <Star value={4} selected={value > 3} setValue={setValue}/>
+            <Star value={5} selected={value > 4} setValue={setValue}/>
 
         </div>
     );
 }
 
 type StarType = {
-    selected:boolean
+    selected: boolean,
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
+    value:  1 | 2 | 3 | 4 | 5
 }
 
-export const Star = (props:StarType)=>{
+export const Star = (props: StarType) => {
     return (
         <>
-            {props.selected ? <span><b>star</b></span>:<span>star</span>}
+            <span onClick={() => {
+                props.setValue(props.value)
+            }}>{props.selected ? <b>star</b> : 'star'}</span>
         </>
     );
 }
